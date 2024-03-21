6a9b4f50e2c56633e893fcee5c051836cffde866
sgrischenko8 <sgrischenko8@gmail.com>
2024-03-22 00:00:33 +0200

    Update Form.styled.js

--- src/components/Form/Form.styled.js
+++ src/components/Form/Form.styled.js
@@ -3,11 +3,23 @@ import styled from 'styled-components';
 export const FormBox = styled.form`
   margin-bottom: 20px;
   padding: 10px;
-  position: absolute;
+  position: relative;
+
+  width: 100%;
   display: flex;
+
+  @media screen and (min-width: 780px) {
+    position: absolute;
+    max-width: 33%;
+
+    & > input {
+      max-width: 100%;
+    }
+  }
 
   & > input {
     padding: 5px 10px 5px 40px;
+    width: 100%;
   }
 
   & > svg {
